da1d731c75537d4706d6193b31efad06a6016001
Adarsh Kenjale <adarsh1729>
2021-11-29 12:51:07 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const pg = require('pg');
+const pool = new pg.Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Test',
+  password: 'Qwerty',
+  port: 5432,
+});
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  
+    
+  pool.query('INSERT INTO users (name,age,country,position,wage) VALUES ($1, $2, $3, $4, $5) RETURNING *', [name, age, country, position, wage], (error, results) => {
+    if (error) {
+      console.log(error);
+    }
+    res.send("Values Inserted");
+  })
+});
+
+app.get("/", (req, res) => {
+ 
+  console.log("testing");
+  
+    pool.query('SELECT * FROM users', (error, results) => {
+      if (error) {
+        console.log(error);
+        
+      }
+     res.send(results);
+    })
+  });
+  
+
+
+app.put("/update", (req, res) => {
+  const name = req.body.name;
+  const wage = req.body.wage;
+  pool.query(
+    "UPDATE users SET wage = $1 WHERE name = $2 ",
+    [wage, name],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:name", (req, res) => {
+  const name = req.params.name;
+  pool.query("DELETE FROM users WHERE name = $1 RETURNING *", [name], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("shit");
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
+});
